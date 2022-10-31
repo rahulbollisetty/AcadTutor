25430b505490dc6202c990a111e6c1e4caddb90a
rahulbollisetty <rahul20bollisetty@gmail.com>
2022-10-31 11:43:56 +0530

    Revert "Revert "basic auth""
    This reverts commit 23f980ec21de63780e841f7b88fb50ff301d92a0.

---
+++ backend/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('register',views.register),
+    path('csrf_cookie',views.get_CSRF_token),
+    path('login',views.Login),
+    path('authenticated',views.IsAuthenticated),
+    path('logout',views.Logut)
+]
