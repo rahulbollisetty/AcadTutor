97d04da3865ba788aaa1b475c94a5ef9525b1497
rahulbollisetty <rahul20bollisetty@gmail.com>
2022-11-13 19:05:13 +0530

    del

--- frontend/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import {showLoginSlice} from '../features/Reducer'
-
-export const store = configureStore({
-  reducer: {
-    showLoginSlice : showLoginSlice,
-},
-})
