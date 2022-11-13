39d01a7db2aff6f1357c9f44bd299b42ae1612b3
rahulbollisetty <rahul20bollisetty@gmail.com>
2022-11-13 19:10:19 +0530

    urls update

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.home),
     path('register',views.register),
     path('csrf_cookie',views.get_CSRF_token),
     path('login',views.Login),
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -6,11 +6,7 @@ from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
 from django.utils.decorators import method_decorator
 from django.contrib.auth import authenticate,login,logout
 from django.http import HttpResponse
-@ensure_csrf_cookie
-@permission_classes([permissions.AllowAny])
-@api_view(('GET',))
-def home(request):
-    return HttpResponse("hello there")
+
 
 @ensure_csrf_cookie
 @permission_classes([permissions.AllowAny])
