b4ea4e2d543aa16767cec247cd886a85b218d8fa
rahulbollisetty <rahul20bollisetty@gmail.com>
2022-11-16 17:32:17 +0530

    changes made to accounts app models

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -41,13 +41,18 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     last_login = models.DateTimeField(null=True, blank=True)
     date_joined = models.DateTimeField(auto_now_add=True)
-
+    is_teach = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
     USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     objects = UserManager()
-
+    def __str__(self):
+        if self.is_student == True:
+            return self.email + " - is_student"
+        else:
+            return self.email + " - is_teacher"
     def get_absolute_url(self):
         return "/users/%i/" % (self.pk)
     def get_email(self):
@@ -56,28 +61,21 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 class HOD(models.Model):
     branch = models.CharField(null=True,blank=True,max_length=10)
     college = models.CharField(null=True,blank=True,max_length=100)
-    referral_code = models.IntegerField(max_length=5)
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE,primary_key=True)
-
-    def get_absolute_url(self):
-        return u'/profile/show/%d' % self.id
-    def generate_verification_code(self):
-        return base64.urlsafe_b64encode(uuid.uuid1().bytes.encode("base64").rstrip())[:5]
-    def save(self, *args, **kwargs):
-        
-        if not self.pk:
-            self.referral_code = self.generate_verification_code()
-        
-
-        return super(HOD, self).save(*args, **kwargs)
-
-class user_type(models.Model):
-    is_teach = models.BooleanField(default=False)
-    is_student = models.BooleanField(default=False)
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE,primary_key=True)
-
+    refid = models.UUIDField(primary_key = True,default = uuid.uuid4,editable = False)    
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     def __str__(self):
-        if self.is_student == True:
-            return CustomUser.get_email(self.user) + " - is_student"
-        else:
-            return CustomUser.get_email(self.user) + " - is_teacher"
+        return CustomUser.get_email(self.user) + " - is_HOD"
+
+class Teacher(models.Model):
+    hod = models.ForeignKey(HOD,on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE,primary_key=True)    
+    def __str__(self):
+        return CustomUser.get_email(self.user) + " - is_teacher"
+
+class Student(models.Model):
+    branch = models.CharField(null=True,blank=True,max_length=10)
+    sem = models.IntegerField(null=True,blank=True,max_length=2)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE,primary_key=True)    
+    def __str__(self):
+        return CustomUser.get_email(self.user) + " - is_student"
+
