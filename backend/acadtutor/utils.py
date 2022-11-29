cf24b420d6624e97a8ad37f62a02c3454f6b9f1b
rahulbollisetty <rahul20bollisetty@gmail.com>
2022-11-29 20:05:43 +0530

    mongodb updated

--- backend/acadtutor/utils.py
+++ backend/acadtutor/utils.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 def get_db_handle():
-    client = MongoClient()
+
+    client = MongoClient("mongodb+srv://rahul:2hUUssKfzcjKkCtB@cluster0.ctsuisv.mongodb.net/?retryWrites=true&w=majority")
     db_handle = client['acadtutor']
     print(db_handle)
     return db_handle, client
