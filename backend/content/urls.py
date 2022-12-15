ac384c29f1e954dcd887ed7eb9080f3b864d1c9e
rahulbollisetty <rahul20bollisetty@gmail.com>
2022-12-15 20:14:30 +0530

    added branch table

--- backend/content/urls.py
+++ backend/content/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('addunit',views.addUnit,name='addunit'),
     path('addsubtopic',views.addSubTopic,name='addsubtopic'),
     path('<str:unit_id>/get_unit',views.getUnit,name='getuint'),
+    path('<str:branch>/get_branch',views.getBranch,name='getbranch'),
+    path('<str:subj_id>/get_subj',views.getSubj,name='getsubj'),
+
 
 ]
